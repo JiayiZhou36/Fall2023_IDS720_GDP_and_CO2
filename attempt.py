47d892837319ffa8ca8bb3dcd63dad226524c6c8
JiayiZhou <jiayizhou@JiayiZhous-MacBook-Pro.local>
2023-09-05 14:40:54 -0400

    new branch

---
+++ attempt.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+gdp = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+
+print(gdp)
