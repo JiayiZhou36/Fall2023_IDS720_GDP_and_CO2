9a4ab1432058378fb6a1959da5c491fe7e0edf4e
JiayiZhou <jiayizhou@JiayiZhous-MacBook-Pro.local>
2023-09-07 14:05:20 -0400

    modified

--- attempt.py
+++ attempt.py
@@ -1,16 +1,18 @@
+# import library
 import pandas as pd
 import matplotlib
 
+# read csv
 gdp = pd.read_csv(
     "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
 )
 
-
-# print(gdp.columns)
+# extract columns
 print(gdp["Mortality rate, infant (per 1,000 live births)"])
 print(gdp["GDP per capita (constant 2010 US$)"])
 print(gdp["Country Name"])
 
+# Plot a scatter plot
 gdp.plot.scatter(
     x="Mortality rate, infant (per 1,000 live births)",
     y="GDP per capita (constant 2010 US$)",
